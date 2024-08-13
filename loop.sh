cb8733fe165272ec380fb9d146c2f6c34ddb4c4d
kelirniaboua <kelirniaboua@gmail.com>
2024-08-13 20:44:55 +0000

    development

---
+++ loop.sh
@@ -0,0 +1,6 @@
+I will go home when I am ready
+What do you think of it
+This is not what I am supposed to do
+From now on, you should focus on yourself
+Deriving from the shell, you'll lose the conent
+
